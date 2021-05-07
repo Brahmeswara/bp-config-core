269f4137b3aa81c25e68696a63c35b7b6429cdf0
Brahmeswara <brahmeswara@yahoo.com>
2021-05-07 04:55:41 -0700

    export doctype services business logic services

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -2,5 +2,9 @@
 const {GroupsService} = require('./core/services/groups-service') ;
 const {GroupsRepositoryInterface} = require('./core/respository/groups-repo');
 const {GroupsDynamodbRepo} = require('./datasources/groups-dynamodb');
+const {DocTypeService} = require('./core/services/doctype-services');
+const {DocTypeRepositoryInterface} = require('./core/respository/doctypes-repo');
+const {DocTypeDynamodbRepo} = require('./datasources/doctype-dynamodb');
 
-export {GroupsService, GroupsRepositoryInterface,  GroupsDynamodbRepo};
+export {GroupsRepositoryInterface,  GroupsDynamodbRepo, GroupsService, 
+        DocTypeRepositoryInterface, DocTypeDynamodbRepo,  DocTypeService};
